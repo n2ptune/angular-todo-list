16816fc7d92979eba267a515baf5e39a8d52ddb4
n2ptune <n2ptune@outlook.com>
2021-08-02 00:15:44 +0900

    lint

--- src/app/todo/todo-detail/todo-detail.component.spec.ts
+++ src/app/todo/todo-detail/todo-detail.component.spec.ts
@@ -1,25 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { TodoDetailComponent } from './todo-detail.component';
+import { TodoDetailComponent } from './todo-detail.component'
 
 describe('TodoDetailComponent', () => {
-  let component: TodoDetailComponent;
-  let fixture: ComponentFixture<TodoDetailComponent>;
+  let component: TodoDetailComponent
+  let fixture: ComponentFixture<TodoDetailComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoDetailComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [TodoDetailComponent]
+    }).compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TodoDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(TodoDetailComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
